6fb02946e9e9156c87257757ea86a2ca07d7f2ab
Oscar Eduardo Fonseca Gomez <edsphinx@gmail.com>
2019-01-19 14:31:13 -0600

    initial version

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const proxy2 = 'http://cors.io/?';
+export const key = 'c9f5d92abeca44135c647c4ba7c2a510';
+export const api = 'https://www.food2fork.com/api/';
+export const api2 = 'https://www.themealdb.com/api/json/v1/1/search.php';
